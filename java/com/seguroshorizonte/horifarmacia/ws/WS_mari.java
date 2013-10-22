c1ae5704af6ea7b6b937e7c286a7f8d5bf6953f6
Mariela Arboleda <luz.arboleda25@gmail.com>
2013-10-22 16:32:59 -0430

    lo mio

--- java/com/seguroshorizonte/horifarmacia/ws/WS_mari.java
+++ java/com/seguroshorizonte/horifarmacia/ws/WS_mari.java
@@ -4,6 +4,11 @@
  */
 package com.seguroshorizonte.horifarmacia.ws;
 
+import com.seguroshorizonte.horifarmacia.entidades.Preorden;
+import com.seguroshorizonte.horifarmacia.entidades.PreordenMedicamento;
+import java.math.BigDecimal;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -15,6 +20,11 @@ import javax.jws.WebParam;
 @WebService(serviceName = "WS_mari")
 public class WS_mari {
 
+    @EJB
+    private com.seguroshorizonte.horifarmacia.sessionfacade.PreordenFacade preordenServices;
+    @EJB
+    private com.seguroshorizonte.horifarmacia.sessionfacade.PreordenMedicamentoFacade preordenMedicamentoServices;
+
     /**
      * This is a sample web service operation
      */
@@ -22,5 +32,21 @@ public class WS_mari {
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
     }
-    
+
+
+
+    @WebMethod(operationName = "obtenerPreordenMedicamentoXIdPreorden")
+    public List< PreordenMedicamento> obtenerPreordenMedicamentoXIdPreorden(@WebParam(name = "idPreorden") String idPreorden) {
+        if (idPreorden == null) {
+            return null;
+        }
+        Preorden Preorden = new Preorden();
+        Preorden.setIdpreorden(new BigDecimal(idPreorden));
+        List<PreordenMedicamento> Resultado = preordenMedicamentoServices.obtenerListadoMedicamentosXIdpreorden(Preorden);
+        if (Resultado != null && !Resultado.isEmpty()) {
+            System.out.println("__________________"+Resultado.get(0).getIdmedicamento().getDescripcion());    ;
+            return Resultado;
+        }
+        return null;
+    }
 }
