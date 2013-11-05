eca1a50ecf5d1bb2e21510099989c71f81032574
Jose fuentes <jose.fuentes@pangeatech.com.ve>
2013-11-05 13:31:00 -0430

    colaPreorden

--- java/com/seguroshorizonte/horifarmacia/sessionfacade/ColaPreordenFacade.java
+++ java/com/seguroshorizonte/horifarmacia/sessionfacade/ColaPreordenFacade.java
@@ -21,9 +21,10 @@ import javax.persistence.Query;
  */
 @Stateless
 public class ColaPreordenFacade extends AbstractFacade<ColaPreorden> {
+
     @PersistenceContext(unitName = "HoriFarmaciaPU")
     private EntityManager em;
-    
+
     @Override
     protected EntityManager getEntityManager() {
         return em;
@@ -32,94 +33,91 @@ public class ColaPreordenFacade extends AbstractFacade<ColaPreorden> {
     public ColaPreordenFacade() {
         super(ColaPreorden.class);
     }
-    
-    public Object obtenerTotalXFechaHoy(){
-        
-        Date fecha=new Date();
+
+    public Object obtenerTotalXFechaHoy() {
+
+        Date fecha = new Date();
         Date fecha2;
         Calendar cal = Calendar.getInstance();
         Calendar cal2 = Calendar.getInstance();
-      
+
         cal.setTime(fecha);
         System.out.print(cal);
         cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MILLISECOND, 0);
-       
-        fecha=cal.getTime();
+
+        fecha = cal.getTime();
         cal2.setTime(fecha);
-        int dia= cal.get(Calendar.DAY_OF_MONTH);
+        int dia = cal.get(Calendar.DAY_OF_MONTH);
         dia++;
         cal2.set(Calendar.DAY_OF_MONTH, dia);
-       
-        fecha2=cal2.getTime();
-        
+
+        fecha2 = cal2.getTime();
+
         Object resultCont;
         Query query = em.createNamedQuery("ColaPreorden.findByColaFechaHoy", ColaPreorden.class);
-        query.setParameter("fecha1",fecha);
-        query.setParameter("fecha2",fecha2);
+        query.setParameter("fecha1", fecha);
+        query.setParameter("fecha2", fecha2);
         resultCont = query.getSingleResult();
         return resultCont;
     }
-    
-    
-      public int contarSHXidAnalista(String IdAnalista) {
-       
-        
-         Query query = em.createNamedQuery("ColaPreorden.findAll", ColaPreorden.class);
-        
+
+    public int contarSHXidAnalista(String IdAnalista) {
+
+
+        Query query = em.createNamedQuery("ColaPreorden.findAll", ColaPreorden.class);
+
 
         List<ColaPreorden> resultList = query.getResultList();
-        
-         String contador= resultList.toString();
-         int con=Integer.parseInt(contador);
+
+        String contador = resultList.toString();
+        int con = Integer.parseInt(contador);
         return con;
 
     }
-     
-     public BigDecimal primeroCola(){
-        
-         
-           Query query = em.createNamedQuery("ColaPreorden.findPrimeroCola", ColaPreorden.class);
-          Object resultList = query.getSingleResult();
-          String Auxi= resultList.toString();
-          BigDecimal aux = new BigDecimal(Auxi);
-         return aux;
-         
-     }
-     
-     
-     public List<ColaPreorden> buscarColaXcodCli(String codCli){
-        
-           
-           Query query = em.createNamedQuery("ColaPreorden.findByCodCli", ColaPreorden.class);
-           query.setParameter("codcli",codCli);
 
-       List<ColaPreorden> resultList =  query.getResultList();
-         return resultList;
-         
-     }
-  
-      public ColaPreorden buscarColaXidPreOrden(String idPreOrden){
-           Preorden Po=new Preorden ();
-           Po.setIdpreorden(new BigDecimal(idPreOrden));       
-           Query query = em.createNamedQuery("ColaPreorden.findByColaIdPreOrden", ColaPreorden.class);
-           query.setParameter("idpreorden",Po);
+    public BigDecimal primeroCola() {
 
-       ColaPreorden resultList = (ColaPreorden) query.getSingleResult();
-         return resultList;
-         
-     }
-      
-       public List<ColaPreorden> Cola(){
-        
-         
-           Query query = em.createNamedQuery("ColaPreorden.findAll", ColaPreorden.class);
-          List<ColaPreorden> resultList = query.getResultList();
-          
-         return resultList;
-         
-     }
-    
+
+        Query query = em.createNamedQuery("ColaPreorden.findPrimeroCola", ColaPreorden.class);
+        Object resultList = query.getSingleResult();
+        String Auxi = resultList.toString();
+        BigDecimal aux = new BigDecimal(Auxi);
+        return aux;
+
+    }
+
+    public List<ColaPreorden> buscarColaXcodCli(String codCli) {
+
+
+        Query query = em.createNamedQuery("ColaPreorden.findByCodCli", ColaPreorden.class);
+        query.setParameter("codcli", codCli);
+
+        List<ColaPreorden> resultList = query.getResultList();
+        return resultList;
+
+    }
+
+    public ColaPreorden buscarColaXidPreOrden(String idPreOrden) {
+        Preorden Po = new Preorden();
+        Po.setIdpreorden(new BigDecimal(idPreOrden));
+        Query query = em.createNamedQuery("ColaPreorden.findByColaIdPreOrden", ColaPreorden.class);
+        query.setParameter("idpreorden", Po);
+
+        ColaPreorden resultList = (ColaPreorden) query.getSingleResult();
+        return resultList;
+
+    }
+
+    public List<ColaPreorden> Cola() {
+
+
+        Query query = em.createNamedQuery("ColaPreorden.findAll", ColaPreorden.class);
+        List<ColaPreorden> resultList = query.getResultList();
+
+        return resultList;
+
+    }
 }
